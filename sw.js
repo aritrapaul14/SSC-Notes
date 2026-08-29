07cc21b80d0296f127dfc468ec9667e7d927afa8
aritrapaul14 <aritra14paul@gmail.com>
2026-08-29 15:39:51 +0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,23 +1,34 @@
-const CACHE_NAME = "ssc-notes-v1";
-
-const FILES = [
-    "./",
-    "./index.html",
-    "./manifest.json"
-];
+const CACHE_NAME = "notemate-ssc-v2";
 
 self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-        .then(cache => cache.addAll(FILES))
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./icon.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
 });
 
 self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request)
-        .then(response => {
-            return response || fetch(event.request);
-        })
-    );
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
